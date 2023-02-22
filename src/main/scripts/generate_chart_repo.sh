62ad9cb9e31f1f7d2e9b1f609760c99bb2eae499
Yevhen Ivantsov <yivantsov@atlassian.com>
2023-02-22 12:29:10 +1100

    User

--- src/main/scripts/generate_chart_repo.sh
+++ src/main/scripts/generate_chart_repo.sh
@@ -49,7 +49,7 @@ echo "Regenerating chart repo index.yaml"
 # release artifacts on GitHub, and add any missing releases to the index.yaml file. The updated file is then left in
 # $PUBLISH_DIR for committing to git.
 docker run \
-  -u root:root \
+  -user "$(id -u):$(id -g)" \
   -v "$(pwd):/index" \
   -v "$(pwd)/$PACKAGE_DIR:/packages" \
   --workdir="/index" \
