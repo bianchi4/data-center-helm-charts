1abfd6ed1f214ec3c99a680df46a54d0cd462fb2
Yevhen Ivantsov <yivantsov@atlassian.com>
2023-02-23 18:01:13 +1100

    Upload key

--- src/main/scripts/generate_chart_repo.sh
+++ src/main/scripts/generate_chart_repo.sh
@@ -28,6 +28,7 @@ for chart in "$CHARTS_SRC_DIR"/*/
   do
     echo "Packaging chart $chart"
     helm package --sign --key 'Yifei Zhang' --keyring ~/.gnupg/secring.gpg "$chart" --destination "$PACKAGE_DIR"
+    cp public_key > "$PACKAGE_DIR"/${chart_pub}.tgz.key
   done
 
 echo "Uploading chart packages as Github releases"
